b402141ffe4f9862ad8d1451b4ca595b6a9f10f8
Geoffrey Sneddon <geoffers@gmail.com>
2007-09-23 23:13:05 +0100

    Initial commit

---
+++ unicode.php
@@ -0,0 +1,92 @@
+<?php
+
+class Unicode
+{
+	const unicode_string = 1;
+	const utf32be_string = 2;
+	private $type;
+	private $data;
+	
+	private function __construct()
+	{
+	}
+	
+	public static function from_utf8($string)
+	{
+		$unicode = new Unicode;
+		
+		if (version_compare(phpversion(), '6', '>='))
+		{
+			$unicode->type = Unicode::unicode_string;
+			if (is_unicode($string))
+			{
+				$unicode->data = $string;
+			}
+			else
+			{
+				$substr_char = unicode_get_subst_char();
+				unicode_set_subst_char((unicode) '\uFFFD');
+				$unicode->data = unicode_decode($string, 'UTF-8', U_CONV_ERROR_SUBST);
+				unicode_set_subst_char($substr_char);
+			}
+		}
+		elseif (extension_loaded('mbstring') && ($unicode->data = @mb_convert_encoding($string, 'UTF-32BE', 'UTF-8')))
+		{
+			$unicode->type = Unicode::utf32be_string;
+		}
+		elseif (extension_loaded('iconv') && ($unicode->data = @iconv('UTF-8', 'UTF-32BE', $string)))
+		{
+			$unicode->type = Unicode::utf32be_string;
+		}
+		else
+		{
+			$unicode->type = Unicode::utf32be_string;
+			$unicode->data = '';
+			
+			$character = array();
+			$remaining = 0;
+			
+			$len = strlen($string);
+			for ($i = 0; $i < $len; $i++)
+			{
+				$value = ord($string[$i]);
+				
+				if (!$remaining)
+				{
+					if ($value <= 0x7F)
+					{
+						$character = $value;
+					}
+					elseif ($value & 0xC0)
+					{
+						$character = ($value & 0x1F) << 6;
+						$remaining = 1;
+					}
+					elseif ($value & 0xE0)
+					{
+						$character = ($value & 0x0F) << 12;
+						$remaining = 2;
+					}
+					elseif ($value & 0xF0)
+					{
+						$character = ($value & 0x07) << 18;
+						$remaining = 3;
+					}
+					else
+					{
+						$character = 0xFFFD;
+					}
+				}
+				else
+				{
+					if ($value & 0x80)
+					{
+						$remaining--;
+						$character |= ($value & 0x3F) << ($remaining * 6);
+					}
+					else
+					{
+						$remaining = 0;
+						$character = 0xFFFD;
+					}
+				}
